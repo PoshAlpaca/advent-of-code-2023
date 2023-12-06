712c6a137b17add1bb15a7c8b9f90b86aaa4e0ea
Nikolai Guyot <nikolai.guyot@moia.io>
2023-12-06 09:04:37 +0100

    Add scaffolding for day 07

---
+++ Sources/Day07.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct Day07: AdventDay {
+  let data: String
+
+  // MARK: - Part 1
+
+  func part1() -> Any {
+    "Todo"
+  }
+
+  // MARK: - Part 2
+
+  func part2() -> Any {
+    "Todo"
+  }
+}
