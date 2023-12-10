393b23f4f465db42dab54e731b2c9e712f67b08e
Nikolai Guyot <nikolai.guyot@moia.io>
2023-12-10 22:22:57 +0100

    Solve day 10 part 2

--- Sources/Day10.swift
+++ Sources/Day10.swift
@@ -2,48 +2,36 @@ import Algorithms
 import Foundation
 
 struct Day10: AdventDay {
-  let data: String
-
   let grid: [[Character]]
 
   init(data: String) {
-    self.data = data
     self.grid = data
       .trimmingCharacters(in: .newlines)
       .components(separatedBy: .newlines)
-      .map { $0.map { $0} }
+      .map { $0.map { $0 } }
   }
 
   // MARK: - Part 1
 
   func part1() -> Any {
-    var start: (line: Int, column: Int)? = nil
-    for i in grid.indices {
-      for j in grid[i].indices {
-        if grid[i][j] == "S" {
-          start = (i, j)
-        }
-      }
-    }
+    guard let start = grid.animalLocation else { fatalError("No animal in grid.") }
 
-    guard let start else { return "Wrong" }
-
-    var lastCell: (line: Int, column: Int) = (0, 0)
-    var currentCell: (line: Int, column: Int) = start
+    var previousCell: (row: Int, column: Int) = (0, 0)
+    var currentCell: (row: Int, column: Int) = start
     var counter = 0
 
     while true {
-      let connected = connectedCells(for: currentCell, in: grid)
+      let connectedCells = connectedCells(for: currentCell, in: grid)
 
-      for cell in connected where lastCell.line != cell.line || lastCell.column != cell.column {
-        lastCell = currentCell
+      for (_, cell) in connectedCells where previousCell.row != cell.row || previousCell.column != cell.column {
+        previousCell = currentCell
         currentCell = cell
         break
       }
 
       counter += 1
 
-      if currentCell.line == start.line && currentCell.column == start.column {
+      if currentCell.row == start.row && currentCell.column == start.column {
         break
       }
     }
@@ -54,32 +42,108 @@ struct Day10: AdventDay {
   // MARK: - Part 2
 
   func part2() -> Any {
-    "Todo"
+    var grid = self.grid
+    cleanUp(grid: &grid)
+
+    var inside = false
+    var directionMode: Direction? = nil
+
+    for i in grid.indices {
+      for j in grid[i].indices {
+        let cell = Cell(rawValue: grid[i][j])!
+        switch cell {
+        case .vertical:
+          inside.toggle()
+        case .ground where !inside:
+          grid[i][j] = "O"
+        case .ground where inside:
+          grid[i][j] = "I"
+        case .northWest where directionMode == .south: // F-J
+          inside.toggle()
+        case .southWest where directionMode == .north: // L-7
+          inside.toggle()
+        case .northEast: // L
+          directionMode = .north
+        case .southEast: // F
+          directionMode = .south
+        default:
+          break
+        }
+      }
+    }
+
+    var count = 0
+    for line in grid {
+      for char in line {
+        if char == "I" {
+          count += 1
+        }
+      }
+    }
+
+    return count
+  }
+
+  func cleanUp(grid: inout [[Character]]) {
+    guard let start = grid.animalLocation else { fatalError() }
+
+    var pipeMask: [[Bool]] = Array(repeating: Array(repeating: false, count: grid[0].count), count: grid.count)
+
+    var lastCell: (row: Int, column: Int) = (0, 0)
+    var currentCell: (row: Int, column: Int) = start
+
+    while true {
+      let connected = connectedCells(for: currentCell, in: grid)
+
+      for (_, cell) in connected where lastCell.row != cell.row || lastCell.column != cell.column {
+        lastCell = currentCell
+        currentCell = cell
+        break
+      }
+
+      pipeMask[currentCell.row][currentCell.column] = true
+
+      if currentCell.row == start.row && currentCell.column == start.column {
+        break
+      }
+    }
+
+    let directions = connectedCells(for: start, in: grid).map(\.direction)
+    let startCell = Cell.inferred(fromConnections: directions)
+    grid[start.row][start.column] = startCell.rawValue
+
+    for i in grid.indices {
+      for j in grid[i].indices {
+        if !pipeMask[i][j] {
+          grid[i][j] = "."
+        }
+      }
+    }
   }
 }
 
-func connectedCells(for cell: (line: Int, column: Int), in grid: [[Character]]) -> [(line: Int, column: Int)] {
+func connectedCells(for cell: (row: Int, column: Int), in grid: [[Character]]) -> [(direction: Direction, cell: (row: Int, column: Int))] {
   let adjacent = adjacentCells(for: cell, in: grid)
 
-  let currentCell = Cell(rawValue: grid[cell.line][cell.column])!
+  let currentCell = Cell(rawValue: grid[cell.row][cell.column])!
 
-  var cells: [(line: Int, column: Int)] = []
+  var cells: [(direction: Direction, cell: (row: Int, column: Int))] = []
   for (direction, otherCell) in adjacent {
-    if currentCell.canConnect(to: Cell(rawValue: grid[otherCell.line][otherCell.column])!, direction: direction) {
-      cells.append(otherCell)
+    if currentCell.canConnect(to: Cell(rawValue: grid[otherCell.row][otherCell.column])!, direction: direction) {
+      cells.append((direction, otherCell))
     }
   }
 
   return cells
 }
 
-func adjacentCells(for cell: (line: Int, column: Int), in grid: [[Character]]) -> [(direction: Direction, cell: (line: Int, column: Int))] {
+func adjacentCells(for cell: (row: Int, column: Int), in grid: [[Character]]) -> [(direction: Direction, cell: (row: Int, column: Int))] {
   [
-    (.north, (cell.line - 1, cell.column)),
-    (.south, (cell.line + 1, cell.column)),
-    (.west, (cell.line, cell.column - 1)),
-    (.east, (cell.line, cell.column + 1)),
-  ].filter { grid.indices.contains($1.line) && grid[0].indices.contains($1.column) }
+    (.north, (cell.row - 1, cell.column)),
+    (.south, (cell.row + 1, cell.column)),
+    (.west, (cell.row, cell.column - 1)),
+    (.east, (cell.row, cell.column + 1)),
+  ].filter { grid.indices.contains($1.row) && grid[0].indices.contains($1.column) }
 }
 
 enum Cell: Character {
@@ -92,21 +156,71 @@ enum Cell: Character {
   case ground = "."
   case animal = "S"
 
-  static let northConnectors: Set<Cell> = [.vertical, .northEast, .northWest, .animal]
-  static let southConnectors: Set<Cell> = [.vertical, .southEast, .southWest, .animal]
-  static let eastConnectors: Set<Cell> = [.horizontal, .northEast, .southEast, .animal]
-  static let westConnectors: Set<Cell> = [.horizontal, .northWest, .southWest, .animal]
+  private static let northConnectors: Set<Cell> = [.vertical, .northEast, .northWest, .animal]
+  private static let southConnectors: Set<Cell> = [.vertical, .southEast, .southWest, .animal]
+  private static let eastConnectors: Set<Cell> = [.horizontal, .northEast, .southEast, .animal]
+  private static let westConnectors: Set<Cell> = [.horizontal, .northWest, .southWest, .animal]
+
+  var isNorthConnector: Bool {
+    Self.northConnectors.contains(self)
+  }
+
+  var isSouthConnector: Bool {
+    Self.southConnectors.contains(self)
+  }
+
+  var isEastConnector: Bool {
+    Self.eastConnectors.contains(self)
+  }
+
+  var isWestConnector: Bool {
+    Self.westConnectors.contains(self)
+  }
 
   func canConnect(to otherCell: Cell, direction: Direction) -> Bool {
     switch direction {
-    case .north: Self.northConnectors.contains(self) && Self.southConnectors.contains(otherCell)
-    case .east: Self.eastConnectors.contains(self) && Self.westConnectors.contains(otherCell)
-    case .south: Self.southConnectors.contains(self) && Self.northConnectors.contains(otherCell)
-    case .west: Self.westConnectors.contains(self) && Self.eastConnectors.contains(otherCell)
+    case .north: self.isNorthConnector && otherCell.isSouthConnector
+    case .east: self.isEastConnector && otherCell.isWestConnector
+    case .south: self.isSouthConnector && otherCell.isNorthConnector
+    case .west: self.isWestConnector && otherCell.isEastConnector
+    }
+  }
+}
+
+extension Cell {
+  static func inferred(fromConnections directions: [Direction]) -> Cell {
+    switch Set(directions) {
+    case [.north, .south]: .vertical
+    case [.east, .west]: .horizontal
+    case [.north, .east]: .northEast
+    case [.north, .west]: .northWest
+    case [.south, .east]: .southEast
+    case [.south, .west]: .southWest
+    default: fatalError("Unsupported set of directions: \(directions).")
     }
   }
 }
 
 enum Direction {
   case north, east, south, west
+}
+
+extension [[Character]] {
+  var prettyPrinted: String {
+    self.map { String($0) }.joined(separator: "\n")
+  }
+}
+
+extension [[Character]] {
+  var animalLocation: (row: Int, column: Int)? {
+    for i in self.indices {
+      for j in self[i].indices {
+        if self[i][j] == Cell.animal.rawValue {
+          return (i, j)
+        }
+      }
+    }
+
+    return nil
+  }
 }
--- Tests/Day10.swift
+++ Tests/Day10.swift
@@ -3,7 +3,11 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day10Tests: XCTestCase {
-  // Smoke test data provided in the challenge question
+//  .....
+//  .F-7.
+//  .|.|.
+//  .L-J.
+//  .....
   let testData1 = """
     -L|F7
     7S-7|
@@ -12,12 +16,42 @@ final class Day10Tests: XCTestCase {
     L|-JF
     """
 
+//  ..F7.
+//  .FJ|.
+//  FJ.L7
+//  |F--J
+//  LJ...
   let testData2 = """
     7-F7-
     .FJ|7
     SJLL7
     |F--J
     LJ.LJ
+    """
+
+  let testData3 = """
+    ...........
+    .S-------7.
+    .|F-----7|.
+    .||.....||.
+    .||.....||.
+    .|L-7.F-J|.
+    .|..|.|..|.
+    .L--J.L--J.
+    ...........
+    """
+
+  let testData4 = """
+    .F----7F7F7F7F-7....
+    .|F--7||||||||FJ....
+    .||.FJ||||||||L7....
+    FJL7L7LJLJ||LJ.L-7..
+    L--J.L7...LJS7F-7L7.
+    ....F-J..F7FJ|L7L7L7
+    ....L7.F7||L7|.L7L7|
+    .....|FJLJ|FJ|F7|.LJ
+    ....FJL-7.||.||||...
+    ....L---J.LJ.LJLJ...
     """
 
   func test_part1_1() {
@@ -53,8 +87,23 @@ final class Day10Tests: XCTestCase {
     XCTAssertEqual(String(describing: challenge.part1()), "8")
   }
 
-  func test_part2() {
+  func test_part2_1() {
     let challenge = Day10(data: testData1)
-    XCTAssertEqual(String(describing: challenge.part2()), "Todo")
+    XCTAssertEqual(String(describing: challenge.part2()), "1")
+  }
+
+  func test_part2_2() {
+    let challenge = Day10(data: testData2)
+    XCTAssertEqual(String(describing: challenge.part2()), "1")
+  }
+
+  func test_part2_3() {
+    let challenge = Day10(data: testData3)
+    XCTAssertEqual(String(describing: challenge.part2()), "4")
+  }
+
+  func test_part2_4() {
+    let challenge = Day10(data: testData4)
+    XCTAssertEqual(String(describing: challenge.part2()), "8")
   }
 }
